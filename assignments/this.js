bc2fcd525a5216c474225fbd07035215cd763b2f
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-12 16:01:47 -0700

    prototype done, working on this questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,11 +1,19 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
+* 1. Window/Global object binding
+    in this context, 'this' is pointing to the window or console object
+
+* 2. Implicit Binding
+    When a function is called by a preceding dot, the object before the dot is what 'this' is referring to in the function
+
+* 3. New Binding
+    A constructor function, which creates a new object, is used. The "this" points to the constructor function, and the keyword "new" is used to call the function (and subsequently create a new object).
+
+* 4. Explicit Binding
+    A .call or .apply method can be used to explicitly define 'this' and override the constructor objects.
+
+
 * write out a code example of each explanation above
 */
 
